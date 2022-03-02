9c515261e58b06650ab90112713de7b6aad08a9d
Shayel8 <Shayel2210>
2022-03-02 19:14:22 +0200

    migrate to config values

--- app.py
+++ app.py
@@ -3,24 +3,24 @@ import json
 import flask
 
 from src.config_parser import get_base_config, get_dev_config, get_prod_config
+from src.config import PORT, PROD_ENDPOINT, DEV_ENDPOINT
 
 app = flask.Flask(__name__)
-
 base_config = get_base_config()
 
 
-@app.route('/prod/config/', methods=['GET'])
+@app.route(PROD_ENDPOINT, methods=['GET'])
 def expose_prod_config():
     prod_config = get_prod_config()
     exposed_config = {**base_config, **prod_config}
     return json.dumps(exposed_config)
 
 
-@app.route('/dev/config/', methods=['GET'])
+@app.route(DEV_ENDPOINT, methods=['GET'])
 def expose_dev_config():
     dev_config = get_dev_config()
     exposed_config = {**base_config, **dev_config}
     return json.dumps(exposed_config)
 
 
-app.run()
+app.run(port=PORT)
